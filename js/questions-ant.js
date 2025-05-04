2b6c711396f8ec001b24a4888e6cf8ba9e54b017
Sigillu <133984240+sigillu@users.noreply.github.com>
2025-05-04 10:37:06 -0400

    Delete js/questions-ant.js

--- js/questions-ant.js
+++
@@ -1,4 +0,0 @@
-export const wordCategories = {
-export const allQuizQuestions = [
-export const bonusCountries = ["Argentina", "Brazil", "Canada", "Colombia", "Peru"];
-export const bonusCorrectCountry = "Peru";
