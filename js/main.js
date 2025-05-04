b06f6e8a1071a27ed5dd0b7d31c4bfeaf2bbbd82
Sigillu <133984240+sigillu@users.noreply.github.com>
2025-05-03 23:48:59 -0400

    Delete js/main.js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-import { initializeGame, toggleDarkMode } from './game.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    initializeGame();
-    const toggleBtn = document.getElementById('darkModeToggle');
-    if (toggleBtn) {
-        toggleBtn.addEventListener('click', toggleDarkMode);
-    }
-});
